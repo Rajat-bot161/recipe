cd7d21e0f395a3ace6118c8cdf1072dda032a29e
Rajat-bot161 <68734571+Rajat-bot161@users.noreply.github.com>
2020-09-11 16:22:31 +0530

    Add files via upload

--- data-storage.service.ts
+++ data-storage.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
-import {HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams } from '@angular/common/http';
 import { RecipeService} from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
-import { map ,tap} from 'rxjs/operators';
+import { map ,tap, take, exhaustMap} from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService{
-    constructor(private http:  HttpClient,private recipeService: RecipeService) {}
+    constructor(private http:  HttpClient,private recipeService: RecipeService,private authService: AuthService) {}
 
 
     storeRecipes() {
@@ -20,11 +21,13 @@ export class DataStorageService{
     }
 
     fetchRecipes() {
-       return this.http
+      
+            return this.http
         .get<Recipe[]>(
-            'https://recipe-book-84b4a.firebaseio.com/recipes.json')
-        .pipe(
-            map(recipes => {
+            'https://recipe-book-84b4a.firebaseio.com/recipes.json'
+            )
+       .pipe(
+        map(recipes => {
             return recipes.map(recipe =>{
                 return {
                     ...recipe,
@@ -35,7 +38,9 @@ export class DataStorageService{
         tap(recipes => {
             this.recipeService.setRecipes(recipes);
         })
-        )
+        );
+       
+    }   
         
-    }
+    
 }
